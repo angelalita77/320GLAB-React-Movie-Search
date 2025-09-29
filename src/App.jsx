f75cf548429a270c62fdf2d4611003cdf8aaa86f
angelalita77 <anjeanette@gmail.com>
2025-09-29 18:29:40 -0400

    Imported and used the 'Form' and 'MovieDisplay' components in 'App.jsx'

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import MovieDisplay from './components/MovieDisplay'
+import Form from './components/Form'
+// Import components
+
 
 
 function App() {
 
 
   return (
-    <>
+
+    <div className='App'>
+      <Form/>
+      <MovieDisplay />
+
+    </div>
     
     
-    </>
+
   )
 }
 
